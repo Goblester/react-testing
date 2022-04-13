c25cd810540b66d114c440f34f4e4073f34cf150
Goblester <goblin829@gmail.com>
2022-04-13 18:04:32 +0300

    use controlled inputs

--- src/components/Converter/Converter.tsx
+++ src/components/Converter/Converter.tsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import S from './Converter.module.css';
 
 export const Converter: React.FC = () => {
+    const [rub, setRub] = useState(100)
+    const [usd, setUsd] = useState(1)
 
     return (
         <form className={S.converter}>
             <label>
                 <p>Сумма в рублях</p>
-                <input type={'number'} name={'rub'} min={0} step={'1'}/>
+                <input type={'number'} name={'rub'} min={0} step={'1'}
+                       value={rub}
+                       onChange={(e) => setRub(+e.currentTarget.value)}
+                />
             </label>
 
             <label>
                 <p>Сумма в долларах</p>
-                <input type={'number'} name={'usd'} min={0} step={'1'}/>
+                <input type={'number'} name={'usd'} min={0} step={'1'}
+                       value={usd}
+                       onChange={(e) => setUsd(+e.currentTarget.value)}
+                />
             </label>
         </form>
     )
